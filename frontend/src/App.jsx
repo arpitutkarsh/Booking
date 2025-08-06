010b600b2ef1a9414ba79b6b6452a5d654af126b
arpitutkarsh <91783218+arpitutkarsh@users.noreply.github.com>
2025-08-06 17:34:26 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React  from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './pages/about'
+import About from './pages/About'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
 import Contact from './pages/contact'
